7698c6c45ae7e723ce8bf287ee3d961f7f2ff584
odlmarce <delmarcelle.olivier@gmail.com>
2024-05-27 19:24:13 +0200

    revert minify and added springConstant to edge fields

--- dist/vis-network.esm.js
+++ dist/vis-network.esm.js
@@ -5,7 +5,7 @@
  * A dynamic, browser-based visualization library.
  *
  * @version 0.0.0-no-version
- * @date    2024-05-27T15:40:27.708Z
+ * @date    2024-05-27T17:21:09.025Z
  *
  * @copyright (c) 2011-2017 Almende B.V, http://almende.com
  * @copyright (c) 2017-2019 visjs contributors, https://github.com/visjs
@@ -30321,6 +30321,7 @@ var Edge = /*#__PURE__*/function () {
         opacity: this.options.color.opacity,
         hidden: this.options.hidden,
         length: this.options.length,
+        springConstant: this.options.springConstant,
         shadow: this.options.shadow.enabled,
         shadowColor: this.options.shadow.color,
         shadowSize: this.options.shadow.size,
@@ -30906,7 +30907,7 @@ var Edge = /*#__PURE__*/function () {
       var allowDeletion = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : false;
       var globalOptions = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : {};
       var copyFromGlobals = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : false;
-      var fields = ['endPointOffset', 'arrowStrikethrough', 'id', 'from', 'hidden', 'hoverWidth', 'labelHighlightBold', 'length', 'line', 'opacity', 'physics', 'scaling', 'selectionWidth', 'selfReferenceSize', 'selfReference', 'to', 'title', 'value', 'width', 'font', 'chosen', 'widthConstraint']; // only deep extend the items in the field array. These do not have shorthand.
+      var fields = ['endPointOffset', 'arrowStrikethrough', 'id', 'from', 'hidden', 'hoverWidth', 'labelHighlightBold', 'length', 'springConstant', 'line', 'opacity', 'physics', 'scaling', 'selectionWidth', 'selfReferenceSize', 'selfReference', 'to', 'title', 'value', 'width', 'font', 'chosen', 'widthConstraint']; // only deep extend the items in the field array. These do not have shorthand.
 
       selectiveDeepExtend(fields, parentOptions, newOptions, allowDeletion); // Only use endPointOffset values (from and to) if it's valid values
 
